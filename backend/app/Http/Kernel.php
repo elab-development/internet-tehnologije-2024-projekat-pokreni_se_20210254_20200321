e79de6a53e3227114143a7141e010c8f1da2a102
masagrubor <153126975+masagrubor@users.noreply.github.com>
2025-06-25 20:05:56 +0200

    Add middleware for authentication and role-based access

---
+++ backend/app/Http/Kernel.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http;
+
+use Illuminate\Foundation\Http\Kernel as HttpKernel;
+
+class Kernel extends HttpKernel
+{
+    protected $middleware = [
+        \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
+        \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
+        \App\Http\Middleware\IsAdmin::class,
+        'api' => [
+        \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+        'throttle:api',
+        \Illuminate\Routing\Middleware\SubstituteBindings::class,
+],
+
+    ];
+
+    protected $routeMiddleware = [
+        'auth' => \App\Http\Middleware\Authenticate::class,
+        'is_registered_user' => \App\Http\Middleware\IsRegisteredUser::class,
+        'is_admin' => \App\Http\Middleware\IsAdmin::class,
+        
+    ];
+
+    protected $middlewareGroups = [
+        'api' => [
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            'throttle:api',
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        ],
+    ];
+    
+
+
+}
